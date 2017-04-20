b37946b698e1417c278ac97b8d0a3d1e8a154db4
Pooria <pjrashidi@gmail.com>
2017-04-20 14:39:04 -0700

    added eslint and dependencies and standard and react-standard rules
    also added the eslint loader to webpack so it lints before bundling

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,15 @@ module.exports = {
   entry: './js/ClientApp.js',
   output: {
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
   }
 }
