ad13c94426a181381c682dc2b9cba325095f2fe6
Pooria <pjrashidi@gmail.com>
2017-04-21 17:40:39 -0700

    added nodemon

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const server = express()
+const PORT = 3000
 const key = process.env.API_KEY
-
 
 server.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-server.listen(3000, () => {
-  console.log(key)
+server.listen(PORT, () => {
+  console.log('Listening on PORT', PORT)
 })
