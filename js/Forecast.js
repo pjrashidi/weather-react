a6914116fe2bc7073b1b1a01d1446e8866a511b4
Pooria <pjrashidi@gmail.com>
2017-04-22 13:40:37 -0700

    added conditional to forecast app

--- js/Forecast.js
+++ js/Forecast.js
@@ -4,21 +4,26 @@ import axios from 'axios'
 class Forecast extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {forecast: 'null'}
+    this.state = {forecast: null}
   }
   componentDidMount () {
     axios.get('http://localhost:3000/forecast')
       .then((response) => {
-        this.setState({forecast: JSON.stringify(response.data, null, 4)})
-        console.log(this.state)
+        this.setState({forecast: response.data})
       })
       .catch((error) => {
         console.log(error)
       })
   }
   render () {
+    let forecast
+    if (this.state.forecast) {
+      forecast = <pre>{JSON.stringify(this.state.forecast, null, 4)}</pre>
+    } else {
+      forecast = 'Loading Forecast...'
+    }
     return (
-      <pre><code>{this.state.forecast}</code></pre>
+      <div>{forecast}</div>
     )
   }
 }
