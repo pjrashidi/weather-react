97b27772ea241f374997cd3f170cb07ad9fe05ba
Pooria <pjrashidi@gmail.com>
2017-04-27 14:19:36 -0700

    small tweak to forecast markup

--- js/Forecast.js
+++ js/Forecast.js
@@ -5,14 +5,14 @@ import Currently from './Currently'
 class Forecast extends React.Component {
   render () {
     let forecast
-    if (this.props.forecast) {
+    if (this.props.forecast.currently) {
       forecast =
         <div>
           <Currently {...this.props.forecast.currently} />
           <pre>{JSON.stringify(this.props.forecast, null, 4)}</pre>
         </div>
     } else {
-      forecast = 'Loading Forecast...'
+      forecast = 'Please enter an Address'
     }
     return (
       <div>{forecast}</div>
