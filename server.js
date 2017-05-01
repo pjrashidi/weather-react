8065f84743a64d8cacce97301de45d2e450248e4
Pooria <pjrashidi@gmail.com>
2017-04-30 22:22:56 -0700

    forecast data is now stored as a stringified json object in Redis

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 
-const express = require('express')
-const server = express()
-const axios = require('axios')
-const PORT = process.env.PORT
-const darkSkyKey = process.env.DARK_SKY_KEY
+const express = require('express'),
+      server = express(),
+      axios = require('axios'),
+      redis = require("redis"),
+      redisClient = redis.createClient(),
+      PORT = process.env.PORT,
+      darkSkyKey = process.env.DARK_SKY_KEY
 
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -13,8 +15,15 @@ server.use(function(req, res, next) {
 });
 
 server.get('/forecast/:coord', (req, res) => {
+  redisClient.get(req.params.coord, function(err, reply) {
+    if(reply) {
+      console.log(`Forecast for ${req.params.coord} already stored in Redis`)
+    }
+  })
   axios.get(`https://api.darksky.net/forecast/${darkSkyKey}/${req.params.coord}`)
     .then((response) => {
+      console.log('RETRIEVED DATA FROM DARK SKY')
+      redisClient.set(`${req.params.coord}`, JSON.stringify(response.data), 'EX', 900)
       res.send(JSON.stringify(response.data, null, 4))
     })
     .catch((error) => {
