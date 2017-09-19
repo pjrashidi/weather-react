f1d61d8f16f5f71a7d355ec459e617e10649a925
Pooria <pjrashidi@gmail.com>
2017-09-19 16:16:14 -0700

    added mongoDB

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express'),
       axios = require('axios'),
       redis = require("redis"),
       redisClient = redis.createClient(),
+      MongoClient = require('mongodb').MongoClient, 
+      assert = require('assert'),
       PORT = process.env.PORT,
       darkSkyKey = process.env.DARK_SKY_KEY
 
@@ -12,6 +14,17 @@ server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+});
+
+// Connection URL
+var mongoDBurl = 'mongodb://localhost:27017/myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(mongoDBurl, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to mongoDB server");
+
+  db.close();
 });
 
 server.get('/forecast/:coord', (req, res) => {
