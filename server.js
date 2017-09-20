57f110a81bb625271468ab8e18d82981dfabea67
Pooria <pjrashidi@gmail.com>
2017-09-20 02:28:19 -0700

    made get requests with user names store the name in the mongodb

--- server.js
+++ server.js
@@ -5,47 +5,46 @@ const express = require('express'),
       axios = require('axios'),
       redis = require("redis"),
       redisClient = redis.createClient(),
-      MongoClient = require('mongodb').MongoClient, 
+      MongoClient = require('mongodb').MongoClient,
+      mongoDBurl = 'mongodb://localhost:27017/weatherReact', 
       assert = require('assert'),
       PORT = process.env.PORT,
       darkSkyKey = process.env.DARK_SKY_KEY
 
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-// Connection URL
-var mongoDBurl = 'mongodb://localhost:27017/weatherReact';
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 // Use connect method to connect to the server
 MongoClient.connect(mongoDBurl, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to mongoDB server");
-  var users = db.collection('users')
+  assert.equal(null, err)
+  console.log("Connected successfully to mongoDB server")
+  let users = db.collection('users')
   
-  var insertUsers = function(db, callback) {
-    users.insertOne({"user":"bob"}, function(err, result) {
-      console.log("Added bob");
-      callback(result);
-    });
+  let insertUser = function(db, userName, callback ) {
+    users.insertOne({"user": `${userName}`}, function(err, result) {
+      console.log(`Added ${userName}`)
+      callback(result)
+    })
   }
 
-  var findUsers = function(db, callback) {
+  let findUsers = function(db, callback) {
     users.find({}).toArray(function(err, docs) {
-      console.log("Found the following records");
+      console.log("Found the following records")
       console.log(docs)
-      callback(docs);
-    });
+      callback(docs)
+    })
   }
 
-  insertUsers(db, function() {
-    findUsers(db, function() {
-      db.close();
-    });
-  });
-});
+  server.get('/mongodb/:userName', (req, res) => {
+    insertUser(db, req.params.userName, function() {
+      findUsers(db, function() {
+      })
+    })
+  })
+})
 
 server.get('/forecast/:coord', (req, res) => {
   redisClient.get(req.params.coord, function(err, reply) {
