5655eed4cf3d4d588044afd6107ad961469e6de6
Pooria <pjrashidi@gmail.com>
2017-09-21 06:12:38 -0700

    login now spits out an alert message for validation

--- js/registerNewUser.js
+++ js/registerNewUser.js
@@ -10,7 +10,16 @@ export function registerNewUser (username, password) {
       `http://localhost:3000/mongodb/registerNew/${JSON.stringify(newUserData)}`
     )
     .then(response => {
-      console.log('success', response.data)
+      let alertMessage = ''
+      if (!response.data.username) alertMessage += 'please input username\n'
+      if (!response.data.password) alertMessage += 'please input password\n'
+      if (response.data.username && !response.data.userAvailable) {
+        alertMessage += 'username already taken\n'
+      }
+      if (response.data.username && response.data.userAvailable) {
+        alertMessage += 'new user registered\n'
+      }
+      window.alert(alertMessage)
     })
     .catch(error => {
       console.log(error)
